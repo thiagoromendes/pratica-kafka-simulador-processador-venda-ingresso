d1aa6526de991f9a7ff98087769d20d567528dea
Thiago Mendes <thiagoromendes@gmail.com>
2021-07-02 18:13:00 -0300

    first commit

---
+++ src/main/java/deserializer/VendaDeserializer.java
@@ -0,0 +1,22 @@
+package deserializer;
+
+import org.apache.kafka.common.serialization.Deserializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import model.Venda;
+
+public class VendaDeserializer implements Deserializer<Venda> {
+
+	@Override
+	public Venda deserialize(String topic, byte[] venda) {
+
+		try {
+			return new ObjectMapper().readValue(venda, Venda.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
---
+++ src/main/java/service/ProcessadorVendas.java
@@ -0,0 +1,50 @@
+package service;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.Random;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import deserializer.VendaDeserializer;
+import model.Venda;
+
+public class ProcessadorVendas {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		Properties properties = new Properties();
+		properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, VendaDeserializer.class.getName());
+		properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		properties.put(ConsumerConfig.GROUP_ID_CONFIG, "group-processamento");
+		properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		
+		try(KafkaConsumer<String, Venda> consumer = new KafkaConsumer<String, Venda>(properties)){
+			
+			consumer.subscribe(Arrays.asList("venda-ingresso"));
+			
+			while (true) {
+				ConsumerRecords<String, Venda> vendas = consumer.poll(Duration.ofMillis(200));
+				for (ConsumerRecord<String, Venda> record : vendas) {
+					Venda venda = record.value();
+					
+					if(new Random().nextBoolean()) {
+						venda.setStatus("APROVADA");
+					} else {
+						venda.setStatus("REPROVADA");
+					}
+					
+					Thread.sleep(500);
+					System.out.println(venda);
+				}
+			}
+		}
+	}
+
+}
